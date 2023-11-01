3f34ad0ae22491f4b303963defcced95ea2ed6f1
Erkadaflown <shinomiyaerka@gmail.com>
2023-11-01 12:44:09 +0800

    lab9

---
+++ lab9_1/app/src/main/java/com/example/lab9_1/Register.java
@@ -0,0 +1,85 @@
+package com.example.lab9_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Register extends AppCompatActivity {
+    DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Initialize the database reference
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+
+        final EditText name = findViewById(R.id.name);
+        final EditText email = findViewById(R.id.email);
+        final EditText mobile = findViewById(R.id.mobile);
+        final EditText password = findViewById(R.id.password);
+        final EditText conpassword = findViewById(R.id.conpassword);
+        final Button registerBtn = findViewById(R.id.registerBtn);
+        final TextView loginNow = findViewById(R.id.loginNow);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String nameTxt = name.getText().toString();
+                final String emailTxt = email.getText().toString();
+                final String mobileTxt = mobile.getText().toString();
+                final String passwordTxt = password.getText().toString();
+                final String conpasswordTxt = conpassword.getText().toString();
+
+                if (nameTxt.isEmpty() || emailTxt.isEmpty() ||
+                        mobileTxt.isEmpty() || passwordTxt.isEmpty() || conpasswordTxt.isEmpty()) {
+                    Toast.makeText(Register.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                } else if (!passwordTxt.equals(conpasswordTxt)) {
+                    Toast.makeText(Register.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                } else {
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.hasChild(mobileTxt)) {
+                                Toast.makeText(Register.this, "Phone is already registered", Toast.LENGTH_SHORT).show();
+                            } else {
+                                DatabaseReference userReference = databaseReference.child(mobileTxt);
+                                userReference.child("name").setValue(nameTxt);
+                                userReference.child("email").setValue(emailTxt);
+                                userReference.child("password").setValue(passwordTxt);
+
+                                Toast.makeText(Register.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(Register.this, MainActivity.class);
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                        }
+                    });
+                }
+            }
+        });
+
+        loginNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register.this, Login.class);
+                finish();
+            }
+        });
+    }
+}
